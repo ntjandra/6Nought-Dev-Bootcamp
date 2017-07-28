3823a41d9b7745a8d0095da82c14792bd62745eb
jiadar <ross@teamengine.org>
2017-07-28 00:35:50 +0000

    adding server postgres dump

---
+++ misc/server-db-dump.sql
@@ -0,0 +1,74 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.5.7
+-- Dumped by pg_dump version 9.5.7
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: feedme
+--
+
+CREATE TABLE users (
+    id integer,
+    name character varying(80),
+    email character varying(80),
+    password character varying(80)
+);
+
+
+ALTER TABLE users OWNER TO feedme;
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: feedme
+--
+
+COPY users (id, name, email, password) FROM stdin;
+1	brian	brian@pizza.com	delicious
+3	cheese	cheese@feedme.com	delicious
+2	cody	cody@feedme.com	delicious
+\.
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
